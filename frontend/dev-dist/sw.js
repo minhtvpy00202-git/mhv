0745424ae19fc61ecc80ee34b1f0b0ec998e077a
Tran Van Minh <minhtvpy00202@gmail.com>
2026-05-11 14:11:40 +0700

    .
    .

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hefh8hksfe8"
+    "revision": "0.mbmm3cr15p"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
