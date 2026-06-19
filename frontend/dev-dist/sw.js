06dbeea3de3ed4a19186db463170bc1193aef58c
Tran Van Minh <minhtvpy00202@gmail.com>
2026-06-19 13:52:52 +0700

    uda

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.8ll1fajgf1k"
+    "revision": "0.90au0ip9i74"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
